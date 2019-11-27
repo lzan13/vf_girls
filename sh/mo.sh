2232f1de2431aaa679cbfe706ba7d3030334e70d
liulizheng <liulizheng@weidian.com>
2019-11-27 17:58:59 +0800

    修改根据 json 创建模型类脚本

--- sh/mo.sh
+++ sh/mo.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
-dart mo.dart
-com.vmloft.develop.app.demo.flutter packages pub run build_runner build --delete-conflicting-outputs
+dart ./mo.dart
+flutter packages pub run build_runner build --delete-conflicting-outputs
